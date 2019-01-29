3057ec7ebf9eea24786dda9fab49b6e2f3d3acb6
asteriskblue <47119412+asteriskblue@users.noreply.github.com>
2019-01-29 00:17:21 -0500

    adjust for mechanics

--- PhotonMan/src/attack.java
+++ PhotonMan/src/attack.java
@@ -2,41 +2,17 @@ import java.awt.Graphics;
 
 public class attack extends object {
 
-	int atk;
-	int end;
-	character Char;
-	// damage to be dealt by attack
-
-	public attack(character c, int type) {
+	public attack(character c) {
 		super(c.sp, c.x + c.w / 2 - 5, c.y + c.h / 2 - 5, 10, 10);
 		// creates 10x10 object at the center of character
-		Char = c;
-		end = x + c.range;
-		currsp = sp.smallobj[type];
+		currsp = sp.smallobj[c.type];
 		// sets sprite based on the type
-		if (c.id)
-			dx = 4;
+		if (c.isGood)
+			dx = c.moveSpeed * 2;
 		else
-			dx = -4;
+			dx = c.moveSpeed * -2;
 		// default move speed
 
-		atk = c.atk;
-		// sets damage to character's attack stat
 	}
-
-	@Override
-	public void draw(Graphics g) {
-		if (Char.id) {
-			if (x >= end) {
-				visible = false;
-			}
-		} else {
-			if (x <= end) {
-				visible = false;
-			}
-		}
-		super.draw(g);
-		// draws self
-
-	}
+	
 }
