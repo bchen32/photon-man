2eefa3d189b6a2ec173cc62eb69e0546f50a64fb
asteriskblue <47119412+asteriskblue@users.noreply.github.com>
2019-01-29 00:19:45 -0500

    updated spritesheet splicing

--- PhotonMan/src/sprites.java
+++ PhotonMan/src/sprites.java
@@ -17,8 +17,8 @@ public class sprites {
 
 	// 0 bullet 1 slice 2 beam 3 yellow orb 4 purple orb
 	BufferedImage[] smallobj = new BufferedImage[5];
-	// 0 red 1 blue 2 green 3 purple 4 orange
-	BufferedImage[] icons = new BufferedImage[5];
+	// 0 red 1 blue 2 green 3 purple 4 orange 5 black
+	BufferedImage[] icons = new BufferedImage[6];
 	// 0 metal box 1 wood box 2 laser
 	BufferedImage[] largeobj = new BufferedImage[3];
 
@@ -40,12 +40,13 @@ public class sprites {
 			black[x] = img.getSubimage(40 * x, 200, 40, 40);
 			scientist[x] = img.getSubimage(40 * x, 240, 40, 40);
 			soldier[x] = img.getSubimage(40 * x, 280, 40, 40);
-			smallobj[x] = img.getSubimage(20 * x, 320, 20, 20);
-			icons[x] = img.getSubimage(20 * x, 340, 20, 20);
+			smallobj[x] = img.getSubimage(20 * x, 320, 10, 10);
 		}
-		largeobj[0] = img.getSubimage(100, 160, 40, 40);
-		largeobj[1] = img.getSubimage(140, 160, 40, 40);
-		largeobj[2] = img.getSubimage(180, 160, 20, 40);
+		for(int x = 0; x != 6; x++)
+			icons[x] = img.getSubimage(20 * x, 340, 20, 20);
+		largeobj[0] = img.getSubimage(120, 320, 40, 40);
+		largeobj[1] = img.getSubimage(160, 320, 40, 40);
+		largeobj[2] = img.getSubimage(100, 320, 20, 20);
 		// chops up the sprite sheet into individual sprites then organizes them
 	}
 
