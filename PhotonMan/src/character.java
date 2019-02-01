a806f89193f83e540e507c77ae29c29e1a160419
asteriskblue <47119412+asteriskblue@users.noreply.github.com>
2019-02-01 13:25:11 -0500

    Added state for running

--- PhotonMan/src/character.java
+++ PhotonMan/src/character.java
@@ -1,204 +1,80 @@
+import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Rectangle;
-import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
-public class character extends object {
+public class training {
 
-	int health;
-	// health
-	int damage;
-	// strength of attacks
-	int moveSpeed;
-	// move speed when holding a key
-	int energy;
-	// current energy
-	int energyGain;
-	// energy gained via capsules
-	int energyUse;
-	// energy lost when shooting
-	int stunCount;
-	// current stun (greater than zero means stunned)
-	boolean isGood;
-	// checks if character is Photon Man
-	boolean hasShield;
-	// checks if character has shield active
-	boolean isDead;
-	// checks if the character is dead
-	int type;
-	// type of attack
-	ArrayList<attack> attacks;
-	// stores all attacks
-	int walkCount;
-	// regulates the walk animation
-	BufferedImage profile;
-	// image for profile
+	character player;
+	// player character
+	ArrayList<character> enemies;
+	// enemy characters
+//	int attackCount = 0;
 
-	public character(sprites sp, int x, int y) {
-		super(sp, x, y, 40, 40);
-		// creates 40x40 object at the given location
-		defaultLoadOut();
+	public training(sprites sp) {
+		player = new character(sp, 20, 140);
+		// creates new player at 20, 200
+		player.setLoadOut("blue");
+		enemies = new ArrayList<character>();
+		for (int x = 0; x != 20; x++) {
+			character e = new character(sp, 600 + x * 300, 140);
+			e.setLoadOut("scientist");
+			enemies.add(e);
+		}
+		// creates and instantiates enemies
 	}
 
-	public void defaultLoadOut() {
-		health = 100000;
-		damage = 1;
-		moveSpeed = 2;
-		energy = 100;
-		energyGain = 5;
-		energyUse = 10;
-		stunCount = 0;
-		isGood = true;
-		hasShield = true;
-		isDead = false;
-		type = 0;
-		attacks = new ArrayList<attack>();
-		walkCount = 0;
-		profile = null;
-	}
-
-	public void setLoadOut(String s) {
-		defaultLoadOut();
-		if (s.equals("red")) {
-			mysp = sp.red;
-			profile = sp.icons[0];
-		}
-		if (s.equals("blue")) {
-			mysp = sp.blue;
-			damage = 2;
-			energyUse = 2;
-			type = 1;
-			profile = sp.icons[1];
-		}
-		if (s.equals("green")) {
-			mysp = sp.green;
-			stunCount = -1000000;
-			moveSpeed = 1;
-			type = 2;
-			profile = sp.icons[2];
-		}
-		if (s.equals("purple")) {
-			mysp = sp.purple;
-			energyGain = 10;
-			profile = sp.icons[3];
-		}
-		if (s.equals("orange")) {
-			mysp = sp.orange;
-			moveSpeed = 3;
-			profile = sp.icons[4];
-		}
-		if (s.equals("black")) {
-			mysp = sp.black;
-			damage = 3;
-			hasShield = false;
-			type = 2;
-			profile = sp.icons[5];
-		}
-		if (s.equals("scientist")) {
-			mysp = sp.scientist;
-			health = 1;
-			isGood = false;
-			dx = -2;
-		}
-		if (s.equals("soldier")) {
-			mysp = sp.soldier;
-			health = 2;
-			isGood = false;
-			dx = -2;
-		}
-	}
-
-	@Override
 	public void draw(Graphics g) {
-		for (attack a : attacks)
-			a.draw(g);
-		// draws all attacks
-		super.draw(g);
-		// draws self
+		player.draw(g);
+		// draws player
+		for (character e : enemies) {
+			e.draw(g);
+		}
+		// draws enemies
+		drawOverlay(g);
+	}
+	
+	public void drawOverlay(Graphics g) {
+		g.fillRect(0, 240, 480, 60);
+		g.setColor(Color.WHITE);
+		g.fillRect(60, 260, player.energy, 10);
+		g.drawImage(player.profile, 20, 255, null);
 	}
 
-	@Override
 	public void move() {
-		if(isGood) {
-			if(x + dx < 0 || x + dx > 480 - w) dx = 0;
-			if(y + dy < 0 || y + dy > 240 - h) dy = 0;
+		player.move();
+		// moves player
+//		attackCount++;
+		for (character e : enemies) {
+//			if (attackCount == 20) {
+//				// attacks every 20 frames
+//				attackCount = 0;
+//				e.attack();
+//			}
+			e.move();
 		}
-		if (isDead) {
-			x += dx;
-			// body keeps moving
-		} else {
-			walkAnimation();
-			// adjust walk animation
-			super.move();
-			// moves self
-		}
+		// moves enemies
 		check();
-		// removes invisible attacks regardless of death
-		for (attack a : attacks)
-			a.move();
-		// moves attacks regardless of death
-	}
-
-	public void walkAnimation() {
-		walkCount++;
-		if (walkCount <= 10)
-			currsp = mysp[1];
-		if (walkCount > 10)
-			currsp = mysp[2];
-		if (walkCount > 20)
-			currsp = mysp[3];
-		if (walkCount > 30)
-			currsp = mysp[2];
-		if (walkCount > 40) {
-			currsp = mysp[1];
-			walkCount = 0;
-		}
-	}
-
-	public void attack() {
-		if(energy < energyUse) return;
-		if (!isDead) {
-			attacks.add(new attack(this));
-		}
-		if(isGood) energy -= energyUse;
-		// adds a new attack if alive
-	}
-
-	public boolean checkHits(character c) {
-		if (c.isDead)
-			return false;
-		// doesn't check hits if dead
-		Rectangle hb = c.getHitBox();
-		// creates enemy hitbox
-		boolean check = false;
-		// will automatically return false (no hit)
-		for (int x = 0; x != attacks.size(); x++) {
-			attack a = attacks.get(x);
-			// gets an attack
-			if (hb.intersects(a.getHitBox())) {
-				// checks if attack hit enemy
-				check = true;
-				// will return true (hit confirmed)
-				c.health -= damage;
-				// adjust enemy health
-				attacks.remove(x);
-				// removes attack from attacks
-				x--;
-				// decrements
-			}
-		}
-		return check;
-		// returns success of attack
+		// checks collision and out of bounds
 	}
 
 	public void check() {
-		for (int x = 0; x != attacks.size(); x++) {
-			if (!attacks.get(x).visible) {
-				// checks if attack is out of range
-				attacks.remove(x);
+		for (int x = 0; x != enemies.size(); x++) {
+			if (enemies.get(x).x < -40) {
+				enemies.remove(x);
 				x--;
-				// removes attack from attacks and decrements
 			}
 		}
+		// checks out of bounds
+
+		for (int x = 0; x != enemies.size(); x++) {
+			character e = enemies.get(x);
+			player.checkHits(e);
+			if (e.health <= 0) {
+				e.isDead = true;
+				e.currsp = e.mysp[4];
+			}
+		}
+		// checks collision
 	}
+
 }
