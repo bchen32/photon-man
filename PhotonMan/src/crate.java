5efcc0a602c3646a058bd99dee76ff27a459e541
StormFalcon32 <benchen9708@gmail.com>
2019-02-26 21:58:21 -0500

    Started implementing crate class

---
+++ PhotonMan/src/crate.java
@@ -0,0 +1,14 @@
+
+public class crate extends object {
+
+	public crate(sprites sp, int x, int y, int w, int h) {
+		super(sp, x, y, 40, 40);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public void move() {
+		
+	}
+
+}
